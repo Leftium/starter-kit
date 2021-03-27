dae56542b0baa15ec28c86fcb80c19224bf97117
Leftium <john@leftium.com>
2021-03-27 15:09:01 +0900

    Add pug.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,13 +2,17 @@
 	import Counter from '$lib/Counter.svelte';
 </script>
 
-<main>
-	<h1>Hello world!</h1>
-
-	<Counter />
-
-	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
-</main>
+<template lang="pug">
+	main
+		h1 Hello world!
+		
+		Counter
+		
+		p 
+			| Visit 
+			a(href="https://svelte.dev") svelte.dev
+			|  to learn how to build Svelte apps.
+</template>
 
 <style>
 	:root {
